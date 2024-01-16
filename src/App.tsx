43babe860faa0d899c88f28d7cfea848d59992d7
Monika Luu <monika-luu@hotmail.com>
2024-01-16 18:00:08 +0100

    Add Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import "./App.css";
 import Recipe from "./components/Recipe";
 import { Mealtype } from "./enums/Mealtype";
+import { Measurement } from "./enums/Measurement";
 import { IRecipe } from "./interfaces/IRecipe";
+import { Ingredient } from "./interfaces/Ingredient";
 
 function App() {
+  const mockIngredient: Ingredient = {
+    title: "Cucumber",
+    quantity: 1,
+    measurement: Measurement.slice,
+  };
+
   const mockData: IRecipe = {
     id: "1",
     title: "Taco!",
     mealType: Mealtype.dinner,
     portion: 1,
-    instructions: [],
-    ingredients: [],
-    tips: "",
+    instructions: ["Step 1", "Step 2"],
+    ingredients: [mockIngredient],
+    tips: "Should not heat the tortilla longer than 15 seconds in the microwave",
     img: "https://img.hellofresh.com/f_auto,fl_lossy,q_auto,w_1200/hellofresh_s3/image/HF210603_R13_W29_SE_R12344157-1_KB_Main_low-413ba4c1.jpg",
     review: [],
   };
--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -4,7 +4,29 @@ function Recipe(recipe: IRecipe) {
   return (
     <div>
       <img src={recipe.img} alt="" />
-      <h1>{recipe.title}</h1>
+      <div>
+        <button>Save</button>
+        <button>Add to grocery list</button>
+        <button>Add to week planner</button>
+      </div>
+      <div>
+        <h1>{recipe.title}</h1>
+        <h2>{recipe.mealType}</h2>
+        <h2>Portion: {recipe.portion}</h2>
+        <h2>Instructions</h2>
+        {recipe.instructions.map((instruction) => (
+          <p>{instruction}</p>
+        ))}
+        <h3>{recipe.tips}</h3>
+      </div>
+      <div>
+        <h2>Ingredients</h2>
+        {recipe.ingredients.map((ingredient) => (
+          <p>
+            {ingredient.title}: {ingredient.quantity} {ingredient.measurement}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
